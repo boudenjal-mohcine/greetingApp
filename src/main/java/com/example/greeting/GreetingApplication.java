256e17dcb68b9bf8e56ee4899a36188611c92c8f
mohcine-boudenjal <mohcine.boudenjal@etu.uae.ac.ma>
2023-12-13 09:31:16 +0100

    first commit

---
+++ src/main/java/com/example/greeting/GreetingApplication.java
@@ -0,0 +1,18 @@
+package com.example.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class GreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
