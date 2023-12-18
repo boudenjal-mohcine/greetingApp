7ed1282ff2aea0ad1ffa4e3db0e85f38e3d23bbd
mohcine-boudenjal <mohcine.boudenjal@etu.uae.ac.ma>
2023-12-18 14:54:42 +0100

    add jenkinsfile

--- src/main/java/com/example/greeting/GreetingApplication.java
+++ src/main/java/com/example/greeting/GreetingApplication.java
@@ -3,9 +3,12 @@ package com.example.greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GreetingApplication {
 
     public static void main(String[] args) {
@@ -15,4 +18,5 @@ public class GreetingApplication {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
 }
